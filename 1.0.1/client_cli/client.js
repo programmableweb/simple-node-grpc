6e97a4645791f1d3da04eac186c4712d78d54cd6
Bob Reselman <reselbob@gmail.com>
2020-06-04 14:02:53 -0700

    formatting

--- 1.0.1/client_cli/client.js
+++ 1.0.1/client_cli/client.js
@@ -19,28 +19,32 @@ class SimpleServiceClient {
         this.client = new simplegrpc.SimpleService(serverURL,
             grpc.credentials.createInsecure());
     }
-    add(numbers, callback){
+
+    add(numbers, callback) {
         this.client.Add({numbers}, callback);
     }
-    subtract(numbers, callback){
+
+    subtract(numbers, callback) {
         this.client.subtract({numbers}, callback);
     }
-    divide(numbers, callback){
+
+    divide(numbers, callback) {
         this.client.Divide({numbers}, callback);
     }
-    multiply(numbers, callback){
+
+    multiply(numbers, callback) {
         this.client.Multiply({numbers}, callback);
     }
 
-    chatter({message, count}, callback){
+    chatter({message, count}, callback) {
         const chatItem = message;
         const limit = count;
-        const call = this.client.Chatter({chatItem,limit});
+        const call = this.client.Chatter({chatItem, limit});
         call.on('data', function (response) {
-            callback(null,JSON.stringify(response))
+            callback(null, JSON.stringify(response))
         });
         call.on('end', function () {
-           return;
+            return;
         });
         call.on('error', function (e) {
             console.error(e);
@@ -54,24 +58,24 @@ class SimpleServiceClient {
     @option message: The message to send into the client stream
     @option count: The number of times to blab, [optional]. Default: Infinite
      */
-    blabber({message, count}, callback){
+    blabber({message, count}, callback) {
         let cnt = 0;
         const stream = this.client.Blabber();
         //handler for data incoming from teh server
         stream.on('data', function (response) {
-            callback(null,JSON.stringify(response));
+            callback(null, JSON.stringify(response));
         });
         //when  stream.cancel is called an error gets thrown.
         //It needs to be gobbled.
         stream.on('error', function (err) {
             //Gobble the steam.cancel() error
-            if(err.message !== '1 CANCELLED: Cancelled')throw(err);
+            if (err.message !== '1 CANCELLED: Cancelled') throw(err);
         });
-        //blab is function private to blabber that does the work
+        // blab is function private to blabber that does the work
         // of writing to the client-side stream
         const blab = async () => {
-            //write a JSON object iwth values for the blab and index properties
-            stream.write({blab:message, index:cnt});
+            //write a JSON object with values for the blab and index properties
+            stream.write({blab: message, index: cnt});
             cnt++;
         };
         // interval is a third party library that kicks off
@@ -79,20 +83,21 @@ class SimpleServiceClient {
         // interval of execution to 1000 milliseconds
         interval(async (iteration, stop) => {
             await blab();
-            if (iteration >  count) {
+            if (count && iteration > count) {
                 await stop();
-                //cancel the client stream to turn control back
-                //over the the caller
+                // cancel the client stream to turn control back
+                // over the the caller
                 await stream.cancel();
             }
         }, 1000, {})
     }
 
-    echo(message, callback){
-        callback(null, {result:message});
+    echo(message, callback) {
+        callback(null, {result: message});
     }
-    ping(message, callback){
-        this.client.Ping({data:message}, callback);
+
+    ping(message, callback) {
+        this.client.Ping({data: message}, callback);
     }
 }
 
